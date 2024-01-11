c98959bc12ef5f2858c937c6356eee7ea442760c
Eduardo <echavez@utea.edu.pe>
2024-01-11 10:00:15 -0500

    printf

--- ejemplo2.php
+++ ejemplo2.php
@@ -15,9 +15,17 @@
     </header>
     <section>
         <?php
+        // echo "";
+        // echo '';
             echo "<br>1. Integrar codigo PHP en HTML5";
             echo "<br>2. Página estática versus Página Dinámica";
             echo "<br>3. Manejo de variables en PHP";
+            // printf("mensaje");
+            printf("<br>El sueldo básico es: ");
+            printf("<h3>El Sueldo básico es: </h3>");
+
+            $sueldoBasico = 350;
+            printf("El sueldo básico es: %.3f",$sueldoBasico);
         ?>
     </section>
     <footer>
