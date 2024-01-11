53f7289f384fd5b73209cc52f9618580aecf2bb3
Eduardo <echavez@utea.edu.pe>
2024-01-11 09:05:53 -0500

    Primer commit

---
+++ ejemplo2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - HTML5</title>
+</head>
+
+<body>
+    <header>
+        <h3>Fundamentos de Programación Web</h3>
+    </header>
+    <section>
+        
+    </section>
+    <footer>
+        <h6>Todo los derechos reservados - Men Lee Hector</h6>
+    </footer>
+</body>
+
+</html>
