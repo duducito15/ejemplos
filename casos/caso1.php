5d5559a2c40fdc74f5ef8a3357e4a87d4f411811
Eduardo <echavez@utea.edu.pe>
2024-01-17 10:02:39 -0500

    Caso1

---
+++ casos/caso1.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caso desarrollado 01</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+
+<body>
+    <header>
+        <h3>CASA DE CAMBIOS</h3>
+    </header>
+    <section>
+        <form action="caso1.php" method="get">
+            <table border="0" cellspacing="0" cellpading="0">
+                <tr>
+                    <td>Monto en soles: </td>
+                    <td>
+                        <input type="text" name="txtSoles">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Monto en dolares: </td>
+                    <td>
+                        <input type="text" name="txtDolares">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Monto en Euros: </td>
+                    <td>
+                        <input type="text" name="txtEuros">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Procesar">
+                    </td>
+                    <td>
+                        <input type="reset" value="Limpiar">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+            </table>
+        </form>
+    </section>
+    <footer>
+        <h6>Todos los derechos reservados @Eduardo</h6>
+    </footer>
+</body>
+
+</html>
