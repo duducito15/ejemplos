5d752d6d9f9c5373a3ebb485f4281b9479370ca9
Eduardo <echavez@utea.edu.pe>
2024-01-18 09:53:42 -0500

    caso4 inicio

---
+++ casos/caso4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Salario de Empleados</title>
+    <link rel="stylesheet" href="/estilo.css">
+</head>
+
+<body>
+    <header>
+        <h3>PAGO DE SALARIO DE EMPLEADOS</h3>
+        <img src="banner.jpg" width="600" height="150">
+    </header>
+    <footer>
+        <h6>Todos los derechos reservados @Eduardo</h6>
+    </footer>
+</body>
+
+</html>
