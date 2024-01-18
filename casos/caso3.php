1e2c04ac3865cd586c3227a0075fa823420abc47
Eduardo <echavez@utea.edu.pe>
2024-01-18 08:21:56 -0500

    caso 3 inicio

---
+++ casos/caso3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/estilo.css">
+    <title>Caso 3</title>
+</head>
+<body>
+    <header>
+        <h3>VENTA DE PRODUCTO</h3>
+    </header>
+    <section>
+
+    </section>
+    <footer>
+        <h6>Todos los derechos reservados @Eduardo</h6>
+    </footer>
+</body>
+</html>
