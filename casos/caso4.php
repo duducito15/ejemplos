9c23cdd9250634b670a84944549b8063c6ee7531
Eduardo <echavez@utea.edu.pe>
2024-01-18 10:21:47 -0500

    Caso 4 - formulario completo

--- casos/caso4.php
+++ casos/caso4.php
@@ -11,8 +11,46 @@
 <body>
     <header>
         <h3>PAGO DE SALARIO DE EMPLEADOS</h3>
-        <img src="banner.jpg" width="600" height="150">
+        <img src="../img/empleados.jpg" width="600" height="150">
     </header>
+    <section>
+        <form action="caso4.php" method="post">
+            <table border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td width="150">Empleado: </td>
+                    <td>
+                        <input type="text" name="txtEmpleado" size="60">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Horas: </td>
+                    <td>
+                        <input type="text" name="txtHoras" size="20">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Categoria: </td>
+                    <td>
+                        <select name="selCategoria" >
+                            <option value="jefe"> Jefe</option>
+                            <option value="admisnitrativo"> Admisnitrativo</option>
+                            <option value="operario"> Operario</option>
+                            <option value="practicante"> Practicante</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Calcular" name="btnCalcular">
+                    </td>
+                    <td>
+                        <input type="reset" value="Limpiar" name="btnLimpiar">
+                    </td>
+                </tr>
+            </table>
+
+        </form>
+    </section>
     <footer>
         <h6>Todos los derechos reservados @Eduardo</h6>
     </footer>
