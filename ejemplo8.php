ba5870306b4d0fee2d5f21053e9fade484020f4b
Eduardo <echavez@utea.edu.pe>
2024-01-25 09:20:44 -0500

    ejemplo 8 series

--- ejemplo8.php
+++ ejemplo8.php
@@ -24,15 +24,47 @@
  * 
  */
 
-    $i = 10;
+    /*$i = 10;
     while ($i >= 1) {
         echo $i."<br>";
         $i--;
-    }
+    }*/
 
     //Listar los N primeros pares
+    /*$n = 20;
+    $i = 1;
+    while ($i <= $n) {
+        if ($i % 2 == 0) {
+            echo $i."<br>";
+        }
+        $i++;
+    }*/
 
     //Listar los N primeros elementos de la siguiente serie: 1/2, 3/4, 5/6,..N
-
-
+   /* $n = 20;
+    $i = 1;
+    $num = 1;
+    $den = 2;
+    while ($i <= $n) {
+        echo $num."/".$den."<br>";
+        $num+=2;
+        $den+=2;
+        $i++;
+    }*/
+    // Listar los N primeros elementos de la siguiente serie: 
+    // 1/5, -2/10, 3/15, -4/20..N
+    $n = 20;
+    $i = 1;
+    $num = 1;
+    $den = 5;
+    while ($i <= $n) {
+        if ($i % 2 != 0) {
+            echo $num."/".$den."<br>";
+        }else{
+            echo "-".$num."/".$den."<br>";
+        }
+        $num+=1;
+        $den+=5;
+        $i++;
+    }
 ?>
